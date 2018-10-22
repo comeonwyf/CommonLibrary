3ac81ac4d0f2f86a7167fec6cb9c6d3e11d2681e
wuyufeng <wu_yu_feng@hotmail.com>
2018-10-22 20:28:46 +0800

    commit 自定义倾斜的TextView

--- widgetlibrary/src/main/java/com/example/widgetlibrary/RotateTextView.java
+++ widgetlibrary/src/main/java/com/example/widgetlibrary/RotateTextView.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 
 /**
